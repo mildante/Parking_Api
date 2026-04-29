a91c63aaa66e835c70ed5a56e0234cb7586e0bb2
Milena <milamashukov@gmail.com>
2026-04-29 12:35:26 +0300

    Update 1

--- Parking_Api/Program.cs
+++ Parking_Api/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Parking_Api.Data;
 using Parking_Api.Interfaces;
 using Parking_Api.Services;
+using System.Text;
 using static Parking_Api.Models.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +27,26 @@ builder.Services.AddScoped<IParkingService, ParkingService>();
 builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
 builder.Services.AddScoped<IParkingSessionService, ParkingSessionService>();
 
+var key = Encoding.UTF8.GetBytes("BRHejG4XOmf4iskeung1DvU4aLuN9QLHlxowUA6XWV8Qs5YvRcWCnjhezcK4Bijt");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -37,6 +60,7 @@ app.UseCors(policy =>
           .AllowAnyHeader()
           .AllowAnyMethod());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- Parking_Api/obj/Debug/net8.0/Parking_Api.AssemblyInfo.cs
+++ Parking_Api/obj/Debug/net8.0/Parking_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parking_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e87341acc92153ee466ddc19da1883cc039af4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parking_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parking_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
